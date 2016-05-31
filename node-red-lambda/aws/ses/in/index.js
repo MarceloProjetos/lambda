8dd22aba7f63c4d4af430b3d03e55654618d6f22
Alessandro Holanda <alessandro.holanda@hotmail.com>
2016-05-31 00:37:05 -0300

    add flow route module

--- node-red-lambda/aws/ses/in/index.js
+++ node-red-lambda/aws/ses/in/index.js
@@ -13,11 +13,11 @@ exports.handler = function(event, context) {
     //console.log('Received event:', JSON.stringify(event, null, 2));
 
     // remove unused messages
-    for(var i = event.Records.length - 1; i >= 0; i--) {
+    /*for(var i = event.Records.length - 1; i >= 0; i--) {
         if(!event.Records[i].EventSource === 'aws:sns') {
            event.Records.splice(i, 1);
         }
-    }
+    }*/
 
 	async.forEachOf(event.Records, function(record, key, callback) {
 
@@ -50,7 +50,7 @@ exports.handler = function(event, context) {
 				        ScanIndexForward: false
 				    };	    
 
-					console.log('\n\nFinding SES nodes according to this parameters:\n\n', params);
+					//console.log('\n\nFinding SES nodes according to this parameters:\n\n', params);
 
 		            dynamodb.query(params, function(err, data) {
 
@@ -63,23 +63,23 @@ exports.handler = function(event, context) {
 
 		                } else {
 
-		                    if (data.Items === undefined) {
+		                    if (data.Items.length) {
 
-		                        console.log('\nNodes not found. Forward email to default SES handler');
-
-		                        next('Nodes not found. Forwarded email to default SES handler.');
-
-		                    } else {
-
-		                        //console.log('\nNodes found:\n', JSON.stringify(data.Items, null, 2)); // successful response
+		                        console.log('\nNodes found:\n', JSON.stringify(data.Items, null, 2)); // successful response
 
 		                        next(null, data.Items);
 		                    
+		                    } else {
+
+		                        console.log('\nNodes not found. Forward email to default SES handler');
+
+		                        //next('Nodes not found. Forwarded email to default SES handler.');
+
 		                    }
 		                }
 		            });
 		        },
-		        function forward_msg(nodes, next) {
+		        function forward(nodes, next) {
 
 			        var to_list = [];
 
@@ -107,18 +107,20 @@ exports.handler = function(event, context) {
 				        	default: JSON.stringify(msg)
 				        };
 
-				        console.log('\nSending message:\n', JSON.stringify(msg, null, 2)); // successful response
+				        //console.log('\nSending message:\n', JSON.stringify(msg, null, 2)); // successful response
 
 						sns.publish({
 							Message: JSON.stringify(message),
 							MessageStructure: 'json',
 							TopicArn: topicRoute
-							}, function(err, data) {
+							}, 
+							function(err, data) {
 								if (!err) {
 									console.log('\Successfully dispatch SNS message:\n', JSON.stringify(message, null, 2)); // successful response
 								}
-					            callback2(err, data);
-					    });  
+					        	callback2(err, data);
+					    	}
+					    );  
 
 					}, function(err) {
 			          
@@ -126,7 +128,7 @@ exports.handler = function(event, context) {
 							next('Unable to dispatch all messages due to an error: ' + err);
 						} 
 
-						console.log('Successfully dispatch messages.');
+						//console.log('Successfully dispatch messages.');
 
 						next(null);
 					});
@@ -134,73 +136,19 @@ exports.handler = function(event, context) {
 
 		        }
 		    ], function(err) {
-		        if (err) {
-		            callback2('Unable to notify receipt ' + receipt + ' of incomming SES Mail due to an error: ' + err);
-		        } 
-
-		        callback2(null);
+		        callback2(err);
 		    });
 
 		}, function(err) {
-		  if (err) {
-		    callback('Error: ' + err);
-		  }
-		  callback(null, { msg: 'SES Mail incomming notify successfully.'});          
-
+		  callback(err);
 		});
 
 	}, function(err) {
-	  if (err) {
+	  /*if (err) {
 	    context.fail('Error: ' + err);
-	  }
-	  context.done(null, { msg: 'SES Mail incomming notify successfully.'});          
+	  }*/
+	  context.done(err);          
 
 	});
 
 };
-
-/*
-function uuid() {
-    return Math.random().toString(36).substring(2, 15) +
-        Math.random().toString(36).substring(2, 15);
-}
-
-var Guid = Guid || (function () {
-
-var EMPTY = '00000000-0000-0000-0000-000000000000';
-
-var _padLeft = function (paddingString, width, replacementChar) {
-    return paddingString.length >= width ? paddingString : _padLeft(replacementChar + paddingString, width, replacementChar || ' ');
-};
-
-var _s4 = function (number) {
-    var hexadecimalResult = number.toString(16);
-    return _padLeft(hexadecimalResult, 4, '0');
-};
-
-var _cryptoGuid = function () {
-    var buffer = new window.Uint16Array(8);
-    window.crypto.getRandomValues(buffer);
-    return [_s4(buffer[0]) + _s4(buffer[1]), _s4(buffer[2]), _s4(buffer[3]), _s4(buffer[4]), _s4(buffer[5]) + _s4(buffer[6]) + _s4(buffer[7])].join('-');
-};
-
-var _guid = function () {
-    var currentDateMilliseconds = new Date().getTime();
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (currentChar) {
-        var randomChar = (currentDateMilliseconds + Math.random() * 16) % 16 | 0;
-        currentDateMilliseconds = Math.floor(currentDateMilliseconds / 16);
-        return (currentChar === 'x' ? randomChar : (randomChar & 0x7 | 0x8)).toString(16);
-    });
-};
-
-var create = function () {
-    var hasCrypto = false; //typeof (window.crypto) != 'undefined',
-    var hasRandomValues = false; //typeof (window.crypto.getRandomValues) != 'undefined';
-    return (hasCrypto && hasRandomValues) ? _cryptoGuid() : _guid();
-};
-
-return {
-    newGuid: create,
-    empty: EMPTY
-};})();
-*/
--- node-red-lambda/flow/route/index.js
+++ node-red-lambda/flow/route/index.js
@@ -1,116 +1,25 @@
 "use strict";
 
-var AWS = require('aws-sdk');
-var dynamodb = new AWS.DynamoDB.DocumentClient();
-var sns = new AWS.SNS();
-
 var async = require('async');
-
-var topicArn = 'arn:aws:sns:us-east-1:631712212114:node-red-';
+var flow = require('node-red-aws-lambda-flow-module');
 
 exports.handler = function(event, context) {
     //console.log('Received event:', JSON.stringify(event, null, 2));
 
     // remove unused messages
-    for(var i = event.Records.length - 1; i >= 0; i--) {
+    /*for(var i = event.Records.length - 1; i >= 0; i--) {
         if(!event.Records[i].EventSource === 'aws:sns') {
            event.Records.splice(i, 1);
         }
-    }
+    }*/
 
 	async.forEachOf(event.Records, function(record, key, callback) {
 
-	    var message = JSON.parse(record.Sns.Message)
-
-	    console.log('Received message:\n', message);
-
-	    async.waterfall([
-	        function get(next) {
-
-			    var params = {
-			        TableName : 'node-red-flow',
-			        Key : { 
-			          "id" : message.to
-			        }
-			    };
-
-				console.log('\n\nFinding nodes according to this parameters:\n\n', params);
-
-	            dynamodb.get(params, function(err, data) {
-
-                	console.log('\nNode found:\n', JSON.stringify(data, null, 2));
-
-	                if (err) {
-
-	                  console.log('Err: ', err); // an error occurred
-	                  next(err);
-
-	                } else {
-
-	                    if (data.Item === undefined) {
-
-	                        console.error('\nNode not found.');
-	                        next('Node not found.');
-
-	                    } else {
-
-	                        //console.log('\nNodes found:\n', JSON.stringify(data.Items, null, 2)); // successful response
-
-	                        next(err, { message: message, node: data.Item });
-	                    
-	                    }
-	                }
-	            });
-	        },
-	        function handler(msg, next) {
-
-	        	var message = msg.message;
-	        	var node = msg.node;
-
-	        	console.log('Routing message...', message);
-
-	        	var msg = {
-			          from: message.from || '0000000.0000000',
-			          to: message.to,
-			          type: 'node-red-' + node.type.replace(' ', '-'),
-			          payload: {
-			          	bucket: message.payload.bucket || null,
-			          	key: message.payload.key || null
-			          }
-			          
-		        	}
-
-		        var message = {
-		        	default: JSON.stringify(msg)
-		        };
-
-		        console.log('\nSending message:\n', JSON.stringify(message, null, 2)); // successful response
-
-				sns.publish({
-					Message: JSON.stringify(message),
-					MessageStructure: 'json',
-					TopicArn: topicArn + node.type.replace(' ', '-')
-					}, function(err, data) {
-						if (!err) {
-							console.log('\Successfully sent SNS message to node:\n', topicArn + node.type.replace(' ', '-'), JSON.stringify(message, null, 2)); // successful response
-						}
-			            next(err, data);
-			    });  
-
-	        }
-	    ], function(err) {
-	        if (err) {
-	            callback('Unable to handler message due to an error: ' + err);
-	        } 
-
-	        console.log('Successfully dispatch message.');
-
-	        callback(null);
-	    });
+		flow.route(JSON.parse(record.Sns.Message), callback);
 
 	}, function(err) {
 	  if (err) {
-	    context.fail('Error: ' + err);
+	    context.fail(err);
 	  }
 	  context.done(null, { msg: 'Message dispatch successfully.'});          
 
